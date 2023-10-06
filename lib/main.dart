648e7ba8309a97a48e5ff42e11a3a6a4d1b94dfb
jyunseon <jyunseon81@gmail.com>
2023-10-06 16:55:48 +0900

    first commitgit

---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/Button.dart';
+import 'package:toonflix/widgets/currency_card.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  int counter = 0;
+
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override // flutter에 관한 것이 아니고 dart 프로그래밍 언어에 관한 것임
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF181818),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Hey, Selena',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                )
+              ]),
+              SizedBox(
+                height: 70,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Button(
+                      text: 'transfer',
+                      bgColor: Color(0xfff1b33b),
+                      textColor: Colors.black),
+                  const Button(
+                      text: 'Request',
+                      bgColor: Color(0xfff1b33b),
+                      textColor: Colors.white),
+                ],
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Wallet',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    'view all',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const CurrencyCard(
+                name: 'Euro',
+                code: 'EUR',
+                amount: "6 428",
+                icon: Icons.euro_rounded,
+                isInverted: false,
+              ),
+              Transform.translate(
+                offset: Offset(0, -20),
+                child: const CurrencyCard(
+                  name: 'Bitcoin',
+                  code: 'BTC',
+                  amount: "9 785",
+                  icon: Icons.currency_bitcoin,
+                  isInverted: true,
+                ),
+              ),
+              Transform.translate(
+                offset: Offset(0, -40),
+                child: const CurrencyCard(
+                  name: 'Dollar',
+                  code: 'USD',
+                  amount: "428",
+                  icon: Icons.attach_money_outlined,
+                  isInverted: false,
+                ),
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
